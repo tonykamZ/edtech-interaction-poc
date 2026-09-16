77d8b7315f46fa21ffb8231e1b30d61a815d5f70
tonykamZ <tonykam09@yahoo.com.hk>
2026-09-16 12:50:44 +0800

    Set up github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const base = process.env.GH_PAGES === 'true' ? '/edtech-interaction-poc/' : '/';
   return {
+    base,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
